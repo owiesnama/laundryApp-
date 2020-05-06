4a9db1f24a75116273ac1b0c1d71ff09a57991a0
Monzer <Monzer@altkamul.ae>
2020-05-06 15:51:32 +0200

    Create get,post,put,delete function in request services

--- src/app/services/request/request.service.ts
+++ src/app/services/request/request.service.ts
@@ -1,9 +1,63 @@
 import { Injectable } from '@angular/core';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
+  public serverUrl='';
 
   constructor() { }
+
+  getData(requestBody){
+    return new Promise((resolve,reject) =>{
+      axios.get(this.serverUrl, requestBody)
+      .then(res =>{
+        console.log(res);
+        resolve(res);
+      },err =>{
+        console.log(err);
+        reject(err); 
+      });
+    });
+  } 
+  
+  addData(requestBody){
+    return new Promise((resolve,reject) =>{
+      axios.post(this.serverUrl, requestBody)
+      .then(res =>{
+        console.log(res);
+        resolve(res);
+      },err =>{
+        console.log(err);
+        reject(err); 
+      });
+    });
+  } 
+  
+  editData(requestBody){
+    return new Promise((resolve,reject) =>{
+      axios.put(this.serverUrl, requestBody)
+      .then(res =>{
+        console.log(res);
+        resolve(res);
+      },err =>{
+        console.log(err);
+        reject(err); 
+      });
+    });
+  } 
+  
+  deleteData(requestBody){
+    return new Promise((resolve,reject) =>{
+      axios.delete(this.serverUrl, requestBody)
+      .then(res =>{
+        console.log(res);
+        resolve(res);
+      },err =>{
+        console.log(err);
+        reject(err); 
+      });
+    });
+  }
 }
